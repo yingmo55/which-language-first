9a7a9a85f63f5f6218fa7d6e4cbf49c272a97444
Ying <97061798+yingmo55@users.noreply.github.com>
2022-01-30 18:28:41 -0500

    add background component

---
+++ src/Components/Background/Background.js
@@ -0,0 +1,66 @@
+import React from 'react';
+
+function Background() {
+    return(
+        <div class='body'>
+        <div id='overlay'> </div>
+        <div class="binary-background">
+            <p class="binary">1 1 0 0 1 1 0 0 0 0 0 0     1 1 0 0 0 0 1 1 1 1 1 1</p>
+            <p class="binary">1 1 0 0 0 0 1 1 0 0 0 0 0 0</p>
+            <p class="binary">1 1 1 1 0 0 0 0 1 1 0 0 1 1     1 1 1 1 0 0 1 1 1 1 0 0 0 0</p>
+            <p class="binary">1 1 1 1 0 0 1 1 1 1 0 0 0 0  1 1 1 1 0 </p>
+            <p class="binary">0 1 1 1 1 0 0 0 0     1 1 1 1 0 0 1 1 1 1 1 1 1 1</p>
+            <p class="binary">1 1 0 0 0 0 0 0 0 0 0 0   1 1</p>
+            <p class="binary"> 0 0 1 1 0 0 1 1 1 1 1 1     1 1 1 1 0 0 1 1 1 1 1 1 1 1</p>
+            <p class="binary">1 1 1 1 1 1 0 0 0 0 1 1 0 0</p>
+            <p class="binary">1 1 1 1 0 0 1   1 1 1 0 0 0 0  1 1 1 1 0 0 0</p>
+            <p class="binary"> 0 1 1 0 0 0 0     1 1 0 0 0 0 0 0 0 0 1 1</p>
+            <p class="binary">1 1 0 0 0 0 1 1 1 1 1 1     1 1 0 0 1 1 0 0 0 0 1</p>
+            <p class="binary">1 1 0 0 1 1 0 0 0 0 0 0     1 1 0 0 0 0 1 1 1 1 1 1</p>
+            <p class="binary">1 1 0 0 0 0 1 1 0 0 0 0 0 0</p>
+            <p class="binary">1 1 1 1 0 0 0 0 1 1 0 0 1 1     1 1 1 1 0 0 1 1 1 1 0 0 0 0</p>
+            <p class="binary">1 1 1 1 0 0 1 1 1 1 0 0 0 0  1 1 1 1 0 </p>
+            <p class="binary">0 1 1 1 1 0 0 0 0     1 1 1 1 0 0 1 1 1 1 1 1 1 1</p>
+            <p class="binary">1 1 0 0 0 0 0 0 0 0 0 0   1 1</p>
+            <p class="binary"> 0 0 1 1 0 0 1 1 1 1 1 1     1 1 1 1 0 0 1 1 1 1 1 1 1 1</p>
+            <p class="binary">1 1 1 1 1 1 0 0 0 0 1 1 0 0</p>
+            <p class="binary">1 1 1 1 0 0 1   1 1 1 0 0 0 0  1 1 1 1 0 0 0</p>
+            <p class="binary"> 0 1 1 0 0 0 0     1 1 0 0 0 0 0 0 0 0 1 1</p>
+            <p class="binary">1 1 0 0 0 0 1 1 1 1 1 1     1 1 0 0 1 1 0 0 0 0 1</p>
+            <p class="binary">1 1 0 0 1 1 0 0 0 0 0 0     1 1 0 0 0 0 1 1 1 1 1 1</p>
+            <p class="binary">1 1 0 0 0 0 1 1 0 0 0 0 0 0</p>
+            <p class="binary">1 1 1 1 0 0 0 0 1 1 0 0 1 1     1 1 1 1 0 0 1 1 1 1 0 0 0 0</p>
+            <p class="binary">1 1 1 1 0 0 1 1 1 1 0 0 0 0  1 1 1 1 0 </p>
+            <p class="binary">0 1 1 1 1 0 0 0 0     1 1 1 1 0 0 1 1 1 1 1 1 1 1</p>
+            <p class="binary">1 1 0 0 0 0 0 0 0 0 0 0   1 1</p>
+            <p class="binary"> 0 0 1 1 0 0 1 1 1 1 1 1     1 1 1 1 0 0 1 1 1 1 1 1 1 1</p>
+            <p class="binary">1 1 1 1 1 1 0 0 0 0 1 1 0 0</p>
+            <p class="binary">1 1 1 1 0 0 1   1 1 1 0 0 0 0  1 1 1 1 0 0 0</p>
+            <p class="binary"> 0 1 1 0 0 0 0     1 1 0 0 0 0 0 0 0 0 1 1</p>
+            <p class="binary">1 1 0 0 0 0 1 1 1 1 1 1     1 1 0 0 1 1 0 0 0 0 1</p>
+            <p class="binary">1 1 0 0 1 1 0 0 0 0 0 0     1 1 0 0 0 0 1 1 1 1 1 1</p>
+            <p class="binary">1 1 0 0 0 0 1 1 0 0 0 0 0 0</p>
+            <p class="binary">1 1 1 1 0 0 0 0 1 1 0 0 1 1     1 1 1 1 0 0 1 1 1 1 0 0 0 0</p>
+            <p class="binary">1 1 1 1 0 0 1 1 1 1 0 0 0 0  1 1 1 1 0 </p>
+            <p class="binary">0 1 1 1 1 0 0 0 0     1 1 1 1 0 0 1 1 1 1 1 1 1 1</p>
+            <p class="binary">1 1 0 0 0 0 0 0 0 0 0 0   1 1</p>
+            <p class="binary"> 0 0 1 1 0 0 1 1 1 1 1 1     1 1 1 1 0 0 1 1 1 1 1 1 1 1</p>
+            <p class="binary">1 1 1 1 1 1 0 0 0 0 1 1 0 0</p>
+            <p class="binary">1 1 1 1 0 0 1   1 1 1 0 0 0 0  1 1 1 1 0 0 0</p>
+            <p class="binary"> 0 1 1 0 0 0 0     1 1 0 0 0 0 0 0 0 0 1 1</p>
+            <p class="binary">1 1 0 0 0 0 1 1 1 1 1 1     1 1 0 0 1 1 0 0 0 0 1</p>
+            <p class="binary">1 1 0 0 1 1 0 0 0 0 0 0     1 1 0 0 0 0 1 1 1 1 1 1</p>
+            <p class="binary">1 1 0 0 0 0 1 1 0 0 0 0 0 0</p>
+            <p class="binary">1 1 1 1 0 0 0 0 1 1 0 0 1 1     1 1 1 1 0 0 1 1 1 1 0 0 0 0</p>
+            <p class="binary">1 1 1 1 0 0 1 1 1 1 0 0 0 0  1 1 1 1 0 </p>
+            <p class="binary">0 1 1 1 1 0 0 0 0     1 1 1 1 0 0 1 1 1 1 1 1 1 1</p>
+            <p class="binary">1 1 0 0 0 0 0 0 0 0 0 0   1 1</p>
+            <p class="binary"> 0 0 1 1 0 0 1 1 1 1 1 1     1 1 1 1 0 0 1 1 1 1 1 1 1 1</p>
+            <p class="binary">1 1 1 1 1 1 0 0 0 0 1 1 0 0</p>
+            <p class="binary">1 1 1 1 0 0 1   1 1 1 0 0 0 0  1 1 1 1 0 0 0</p>
+            <p class="binary"> 0 1 1 0 0 0 0     1 1 0 0 0 0 0 0 0 0 1 1</p>
+            <p class="binary">1 1 0 0 0 0 1 1 1 1 1 1     1 1 0 0 1 1 0 0 0 0 1</p>
+        </div>
+    </div>
+    )
+}
