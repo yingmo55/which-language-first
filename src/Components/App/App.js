41c8b756521a0556b9bdde5c1ab506525c15f74f
Ying <97061798+yingmo55@users.noreply.github.com>
2022-01-30 21:07:50 -0500

    Add Title, MessageBox, Button to render function
    Add style to components

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import './App.css';
 import Background from '../Background/Background';
+import MessageBox from '../MessageBox/MessageBox';
+import Button from '../Button/Button';
 
-class App extends React.Component {
-  render(){
+
+function App() {
+
+  const componentMargin = {
+    margin: "10px 10px",
+  }
+
   return (
     <div className="App">
+      <h1>Which Programming Language <br></br>
+        Should I Learn First?</h1>
+      <MessageBox style={componentMargin} />
+      <Button text='test' style={componentMargin}/>
       <Background />
     </div>
   );
-  }
 }
 
 
