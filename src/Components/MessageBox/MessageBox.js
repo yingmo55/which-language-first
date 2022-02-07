47b920f68654395cc5733c8e879adac2feeb5c03
Ying <97061798+yingmo55@users.noreply.github.com>
2022-02-07 15:44:20 -0500

    Add basic MessageBox function and style

--- src/Components/MessageBox/MessageBox.js
+++ src/Components/MessageBox/MessageBox.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './MessageBox.css';
 
 function MessageBox(props) {
+    const content = props.messages;
+    
     return(
         <div className='message' style={props.style}>
-            <p>test</p>
+            {content}
         </div>
     )
 }
