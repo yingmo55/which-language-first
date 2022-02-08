a35952dcbc24f5ab06aeb105ea1e34606d4f5ec2
Ying <97061798+yingmo55@users.noreply.github.com>
2022-02-07 22:53:09 -0500

    Change MessageBox to take props.children instead of specific props

--- src/Components/MessageBox/MessageBox.js
+++ src/Components/MessageBox/MessageBox.js
@@ -2,11 +2,9 @@ import React, {useState} from 'react';
 import './MessageBox.css';
 
 function MessageBox(props) {
-    const content = props.messages;
     
     return(
         <div className='message' style={props.style}>
-            {content}
             {props.children}
         </div>
     )
