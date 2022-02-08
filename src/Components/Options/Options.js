3966e7915423bef5260c1d6e0ad3d3f8f3676c62
Ying <97061798+yingmo55@users.noreply.github.com>
2022-02-07 22:51:31 -0500

    Remove logics from presentational components

--- src/Components/Options/Options.js
+++ src/Components/Options/Options.js
@@ -2,42 +2,15 @@ import React, {useState} from "react";
 import './Options.css';
 
 function Options(props) {
-    const [ jobOrStartUp, setJobOrStartUp ] = useState(null); // job: true startup: false
-    const changeQuestion = props.changeQuestion;
-    let options = Object.keys(props.options);
-    const nextQuestion = Object.values(props.options);
-    const currentIndex = props.currentQuestion;
-
-    const changeIndex = (e) => {
-        const choice = e.target.value
-        const keys = e.target.id
-        const option = options[keys]
-        switch (option){
-            case "Get a job":
-                changeQuestion(choice[0]);
-                setJobOrStartUp(true);
-                break;
-            case "I have a Startup idea!":
-                changeQuestion(choice[0])
-                setJobOrStartUp(false)
-                break;
-            case "web": 
-                jobOrStartUp ? changeQuestion(choice[0]) : changeQuestion(choice[2]);
-                break;
-            default:
-                changeQuestion(choice);
-                break;
-        }
-    }
-
-    options = !jobOrStartUp && currentIndex === '5' ? options.slice(0, options.length - 2) : options ;
+    const options = props.options;
+    const nextQuestion = props.nextQuestions;
 
     const allOptions = options.map((option, index) => 
         <button className="options" 
                 key={index}
                 id={index}
                 value={nextQuestion[index]}
-                onClick={changeIndex} >{option}</button>
+                onClick={props.onClick} >{option}</button>
         )
 
     return (
