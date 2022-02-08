127cd739acaf2e63a1390ccb07d560a4865f229f
Ying <97061798+yingmo55@users.noreply.github.com>
2022-02-07 21:12:09 -0500

    Finish Options Components
    Add function for changing question index
    Add conditions to render different questions/options for certain choices

--- src/Components/Options/Options.js
+++ src/Components/Options/Options.js
@@ -1,22 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
 import './Options.css';
 
 function Options(props) {
+    const [ jobOrStartUp, setJobOrStartUp ] = useState(null); // job: true startup: false
     const changeQuestion = props.changeQuestion;
-    const options = Object.keys(props.options);
+    let options = Object.keys(props.options);
     const nextQuestion = Object.values(props.options);
+    const currentIndex = props.currentQuestion;
 
     const changeIndex = (e) => {
-        console.log(e.target.value)
-        changeQuestion(e.target.value)
+        const choice = e.target.value
+        const keys = e.target.id
+        const option = options[keys]
+        switch (option){
+            case "Get a job":
+                changeQuestion(choice[0]);
+                setJobOrStartUp(true);
+                break;
+            case "I have a Startup idea!":
+                changeQuestion(choice[0])
+                setJobOrStartUp(false)
+                break;
+            case "web": 
+                jobOrStartUp ? changeQuestion(choice[0]) : changeQuestion(choice[2]);
+                break;
+            default:
+                changeQuestion(choice);
+                break;
+        }
     }
 
-    const allOptions = options.map((option, index)=>
+    options = !jobOrStartUp && currentIndex === '5' ? options.slice(0, options.length - 2) : options ;
+
+    const allOptions = options.map((option, index) => 
         <button className="options" 
-                key={index} 
+                key={index}
+                id={index}
                 value={nextQuestion[index]}
                 onClick={changeIndex} >{option}</button>
-    )
+        )
 
     return (
     <>
