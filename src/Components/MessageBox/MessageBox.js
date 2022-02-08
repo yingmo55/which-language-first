3eba97e73254103d539b3f9fd271dc7bb1b21131
Ying <97061798+yingmo55@users.noreply.github.com>
2022-02-07 22:53:39 -0500

    Remove unused import. Adjust formatting

--- src/Components/MessageBox/MessageBox.js
+++ src/Components/MessageBox/MessageBox.js
@@ -1,8 +1,7 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './MessageBox.css';
 
 function MessageBox(props) {
-    
     return(
         <div className='message' style={props.style}>
             {props.children}
