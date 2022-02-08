c331710c7bebbb05d2ae45be9c3ce28511970c0d
Ying <97061798+yingmo55@users.noreply.github.com>
2022-02-07 23:00:55 -0500

    Add basic style

---
+++ src/Components/FirstPage/style.js
@@ -0,0 +1,15 @@
+const componentMargin = {
+    margin: "10px 10px",
+  }
+  
+const changeSize = {
+    width: '100vw',
+    height: '100vh'
+  }
+  
+const zIndex = {
+    zIndex: 3,
+    fontSize: '2.5rem'
+  }
+
+export const styles = {componentMargin, changeSize, zIndex}
