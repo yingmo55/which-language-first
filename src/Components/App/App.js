6a7bc050404e73cfec875ead0bca110bd42fc61f
Sarthak Verma <sarthak.verma@dltlabs.io>
2022-02-12 22:25:32 +0530

    add reset button in results component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,31 +1,35 @@
-import React, {useState} from 'react';
-import './App.css';
-import Background from '../Background/Background';
-import FirstPage from '../FirstPage/FirstPage';
-import QuizContainer from '../../Container/QuizContainer/QuizContainer';
+import React, { useState } from "react";
+import "./App.css";
+import Background from "../Background/Background";
+import FirstPage from "../FirstPage/FirstPage";
+import QuizContainer from "../../Container/QuizContainer/QuizContainer";
 
 function App() {
-
-  const [ isStartQuiz, setIsStartQuiz ] = useState(false);
+  const [isStartQuiz, setIsStartQuiz] = useState(false);
 
   const handleChange = () => {
-    setIsStartQuiz(true)
-  }
+    setIsStartQuiz(true);
+  };
+
+  const handleReset = () => {
+    setIsStartQuiz(false);
+  };
 
   return (
-    <div className='App'>
-      {!isStartQuiz ? <FirstPage handleChange={handleChange} /> : <QuizContainer /> }
+    <div className="App">
+      {!isStartQuiz ? (
+        <FirstPage handleChange={handleChange} />
+      ) : (
+        <QuizContainer handleReset={handleReset} />
+      )}
       <Background />
     </div>
-    
-    )
-
+  );
 }
 /* 
     {!isStartQuiz && <FirstPage isStartQuiz={handleChange}/>}
     {isStartQuiz && <Quiz />}
 
 */
-
 
 export default App;
