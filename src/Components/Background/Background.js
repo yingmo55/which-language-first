ab905cf1708f2941f2953a0979b83ecdddd46459
Ying <97061798+yingmo55@users.noreply.github.com>
2022-02-15 22:45:57 -0500

    Adjust background to render according to screensize
    - Add effect hook to record and update screen size
    - Add ref hook for background div size

--- src/Components/Background/Background.js
+++ src/Components/Background/Background.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, {useRef, useState, useEffect} from 'react';
 import './Background.css';
 import BackgroundBinary from '../BackgroundBinary/BackgroundBinary';
 
 function Background() {
+    const ref = useRef(null);
+    const [width, setWidth] = useState(0);
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        setWidth(ref.current.offsetWidth);
+    }, [screenWidth]);
+
+    useEffect(() => {
+        window.addEventListener('resize', setScreenWidth);
+         
+        return(() => {
+            window.removeEventListener('resize', setScreenWidth);
+        })
+      }, [screenWidth])
+
+    const numOfRender = Math.floor(width / 420) + 1;
+    let backgroundArray = [];
+    for (let i=0; i < numOfRender; i++){
+        backgroundArray.push(<BackgroundBinary key={i} />);
+        }
+   const background = backgroundArray.map(item => item)
+
     return(
     <div id='mainBG'>
-        <div className='background1'>
+        <div className='background1' ref={ref}>
         <div id='overlay'></div>
-        <BackgroundBinary />
-        <BackgroundBinary />
-        <BackgroundBinary />
-        <BackgroundBinary /> 
+        {background}
         </div>
     </div>
     )
